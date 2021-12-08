3b876a61208e71f071b3fa7843e46a45e78bca6d
unknown <adrianoasantunes@gmail.com>
2021-12-08 17:30:33 -0300

    Ajuste novo modulo

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,45 @@
 // ignore: avoid_web_libraries_in_flutter
 //import 'dart:js';
-import 'package:bytebank/screens/transferencia/lista.dart';
+//import 'dart:html';
+//import 'dart:ui';
+//import 'package:bytebank/screens/transferencia/lista.dart';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(const ByteBankApp());
+
+class ByteBankApp extends StatelessWidget {
+  const ByteBankApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Dashboard'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Image.asset('images/bytebank_logo.png'),
+            Container(
+              height: 120,
+              width: 100,
+              color: Colors.green,
+              child: Column(
+                children: const <Widget>[
+                  Icon(Icons.people),
+                  Text('Contatos'),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+/* Conteúdo Alura Curso -> Fundamentos Flutter
 
 class ByteBankApp extends StatelessWidget {
   const ByteBankApp({Key? key}) : super(key: key);
@@ -28,4 +64,4 @@ class ByteBankApp extends StatelessWidget {
     });
     return builder;
   }
-}
+}*/
